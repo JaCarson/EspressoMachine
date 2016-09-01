819598091c64b43e6874122f3e04f2ead8235b0e
JaCarson <JohnAldenCarson@gmail.com>
2016-09-01 19:38:54 -0400

    Initial Commit

---
+++ EspressoMachine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EspressoMachine
+//
+//  Created by John Carson on 9/1/16.
+//  Copyright © 2016 John Carson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
